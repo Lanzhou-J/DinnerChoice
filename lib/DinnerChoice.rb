c768195e2cdb56f7d8db0f06149dd2cdc00a5bc5
Lanzhou Jiang <lzjiang99@gmail.com>
2020-03-10 14:00:41 +1100

    dinner choice gem

---
+++ lib/DinnerChoice.rb
@@ -0,0 +1,8 @@
+require_relative "DinnerChoice/version"
+
+puts 'hello world'
+
+# module DinnerChoice
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
---
+++ lib/DinnerChoice/version.rb
@@ -0,0 +1,3 @@
+module DinnerChoice
+  VERSION = "0.1.0"
+end
